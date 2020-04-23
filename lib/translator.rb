b8982a9b099795a32376ac5c0f650e4b31d5c3fc
Bryce Kennedy <btken88@gmail.com>
2020-04-23 17:58:42 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,7 +17,7 @@ def get_japanese_emoticon(file, emoticon)
     if value[:english] == emoticon
       return value[:japaanese]
   end
-  return
+  return "Sorry, that emoticon was not found"
 end
 
 def get_english_meaning(file, emoticon)
