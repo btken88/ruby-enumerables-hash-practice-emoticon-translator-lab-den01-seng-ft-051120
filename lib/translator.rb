6a356bbb87067c8eea612170232828f421eafe67
Bryce Kennedy <btken88@gmail.com>
2020-04-23 17:54:03 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,23 @@
 # require modules here
+require 'yaml'
 
-def load_library
-  # code goes here
+def load_library(file)
+  emoticons = YAML.load_file(file)
+  emoticon_hash = {}
+  # go through each emoticon to create hash with english & japanese meanings
+  emoticons.each_pair do |key, value|
+    emoticon_hash[key] = {english: value[0], japanese: value[1]}
+  end
+  emoticon_hash
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file, emoticon)
+  hash = load_library(file)
+  p hash[hash.key(emoticon)]
 end
 
 def get_english_meaning
   # code goes here
 end
+
+get_japanese_emoticon('lib/emoticons.yml', "ヽ(ｏ`皿′ｏ)ﾉ")
