57c378e26e48c7545be79354a1ba4c25208d9d15
Bryce Kennedy <btken88@gmail.com>
2020-04-23 18:00:52 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,7 +15,7 @@ def get_japanese_emoticon(file, emoticon)
   hash = load_library(file)
   hash.each_pair do |key, value|
     if value[:english] == emoticon
-      return value[:japaanese]
+      puts value[:japaanese]
     end
   end
   return "Sorry, that emoticon was not found"
