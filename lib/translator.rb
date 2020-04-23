a0c9583ca6e34866b0fdc25d789a73429639cf51
Bryce Kennedy <btken88@gmail.com>
2020-04-23 17:58:07 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,9 +14,9 @@ end
 def get_japanese_emoticon(file, emoticon)
   hash = load_library(file)
   hash.each_pair do |key, value|
-    value[:english] == emoticon
-      
-    end
+    if value[:english] == emoticon
+      return value[:japaanese]
+  end
 end
 
 def get_english_meaning(file, emoticon)
